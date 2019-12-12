b1f1b8add8b349d20e9b2258de9a9b03aeac23c9
Daniel E Rodriguez <dr2d4@live.com>
2019-12-12 04:06:58 -0500

    Update command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # Puppet configuration
 exec { 'config_server':
   provider => shell,
-  command  => 'sudo apt update; sudo apt -y install nginx; echo "Holberton School" | sudo tee /var/www/html/index.html; new_string="server_name _;\n\trewrite ^\/redirect_me http:\/\/dr2d4.tech\/ permanent;"; sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default; sudo nginx -t; sudo service nginx restart',
+  command  => 'sudo apt-get update; sudo apt-get -y install nginx; echo "Holberton School" | sudo tee /var/www/html/index.html; new_string="server_name _;\n\trewrite ^\/redirect_me http:\/\/dr2d4.tech\/ permanent;"; sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-available/default; sudo nginx -t; sudo service nginx restart',
   }
