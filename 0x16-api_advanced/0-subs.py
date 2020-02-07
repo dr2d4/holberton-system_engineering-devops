d6355b77c8b1fb8fa0ce705bce50fba6eb4de26e
Daniel E Rodriguez <dr2d4@live.com>
2020-02-06 19:03:18 -0500

    Task 0 test

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+  Api Reddit
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+      Get JSON of users of a subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {
+        "User-Agent": "My-User-Agent"
+    }
+
+    resp = requests.get(url.format(url), headers=headers, allow_redirects=False)
+
+    if resp.status_code >= 300:
+        return 0
+
+    resp = resp.json()["data"]["subscribers"]
+    return resp
+
