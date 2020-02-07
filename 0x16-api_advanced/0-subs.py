d91c62939dde94e9401602380951f324283aaabd
Daniel E Rodriguez <dr2d4@live.com>
2020-02-06 19:05:47 -0500

    [Fix] - pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,7 @@
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
       Get JSON of users of a subreddit
@@ -14,11 +15,10 @@ def number_of_subscribers(subreddit):
         "User-Agent": "My-User-Agent"
     }
 
-    resp = requests.get(url.format(url), headers=headers, allow_redirects=False)
+    resp = requests.get(url, headers=headers, allow_redirects=False)
 
     if resp.status_code >= 300:
         return 0
 
     resp = resp.json()["data"]["subscribers"]
     return resp
-
