82d384cc7a164fa8c108d1fa7cf5f35400d9f5bb
Daniel E Rodriguez <dr2d4@live.com>
2020-02-06 19:56:44 -0500

    Task 2 test 0001

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -21,18 +21,17 @@ def recurse(subreddit, hot_list=[], count=0, after=None):
     }
 
     sub_info = requests.get(url, params, headers=hs, allow_redirects=False)
-    sub_info = sub_info.json()
 
     if sub_info.status_code >= 400:
         return None
 
+    sub_info = sub_info.json()
+
     hot_l = [child["data"]["title"] for child in sub_info["data"]["children"]]
+    after = sub_info["data"]["after"]
     hot_l += hot_list
 
     if not sub_info["data"]["after"]:
         return hot_l
-
-    count = sub_info["data"]["count"]
-    after = sub_info["data"]["after"]
 
     return recurse(subreddit, hot_l, count, after)
